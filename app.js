6d39b62dbca93b51c36ad1a4da9536ce94068291
Tuguldur <tugo.tmrbtr@gmail.com>
2021-08-23 01:26:03 +0800

    v1.2

--- app.js
+++ app.js
@@ -7,16 +7,19 @@ var scores = [0, 0];
 // Toglogchiin eeljindee tsugluulj baigaa onoog hadgalah huvsagc
 var roundScore = 0;
 
-// Shoonii ali talaaraa buusang hadgalah huvsagc, 1 - 6 hurtelh utga random oor uugnu
-var diceNumber = Math.floor(Math.random() * 6) + 1;
-
 // App ehlehed beldeh
-document.querySelector(".dice").style.display = "none";
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
 
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
 
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
 
-console.log("Dice :" + dice);
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  // Shoonii ali talaaraa buusang hadgalah huvsagc, 1 - 6 hurtelh utga random oor uugnu
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+  diceDom.style.display = "block";
+  diceDom.src = "./img/dice-" + diceNumber + ".png";
+});
