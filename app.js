210301174b7b79dcbeaeada59beb387420df642a
Tuguldur <tugo.tmrbtr@gmail.com>
2021-08-23 01:06:49 +0800

    v1.1

--- app.js
+++ app.js
@@ -1,29 +1,22 @@
-// Toglogchiin eeljiig hadgalah huivsagch, 1r toglogch 0, 2r togloch 1.
+// Toglogchiin eeljiig hadgalah huvsagc, 1r Player 0, 2r Player 1
 var activePlayer = 1;
 
-// Toglogchdiin tsugluulsan onoog hadgalah huivsagch
+// Toglogchdiin tsugluulsan onoog hadgalah huvsagc
 var scores = [0, 0];
 
-// Toglogchiin eeljindee tugluulj baigaa onoog hadgalah huivsagch
+// Toglogchiin eeljindee tsugluulj baigaa onoog hadgalah huvsagc
 var roundScore = 0;
 
-// Shoonii ali talaar buusniig hadgalah huivsagch, 1-6 gesen utgiig ene huivsagchid randomoor uusgej ugnu
-// var diceNumber = Math.floor(Math.random() * 6) + 1;
+// Shoonii ali talaaraa buusang hadgalah huvsagc, 1 - 6 hurtelh utga random oor uugnu
+var diceNumber = Math.floor(Math.random() * 6) + 1;
 
-// Programm ehlehed beltgey
-document.getElementById("score-0").textContent = "0";
-document.getElementById("score-1").textContent = "0";
-document.getElementById("current-0").textContent = "0";
-document.getElementById("current-1").textContent = "0";
+// App ehlehed beldeh
+document.querySelector(".dice").style.display = "none";
 
-var diceDOM = document.querySelector(".dice");
-diceDOM.style.display = "none";
+document.querySelector("#score-0").textContent = 0;
+document.querySelector("#score-1").textContent = 0;
 
-//                                                         Anonymous Function
-document.querySelector(".btn-roll").addEventListener("click", function () {
-  var diceNumber = Math.floor(Math.random() * 6) + 1;
+document.querySelector("#current-0").textContent = 0;
+document.querySelector("#current-1").textContent = 0;
 
-  // Dice iin zurag solih
-  diceDOM.style.display = "block™";
-  diceDOM.src = "./img/dice-" + diceNumber + ".png";
-});
+console.log("Dice :" + dice);
